e8ef91328efb62052bfa7070282ee2e2f29bdf4e
Olivier Sputael <sputael.o@gmail.com>
2023-02-04 16:15:49 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import { inter, nunito, number } from '@fonts';
+import { body, title } from '@fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Head>
                 <title>App Title</title>
             </Head>
-            <main className={`${inter.variable} ${nunito.variable} ${number.variable}`}>
+            <main className={`${body.variable} ${title.variable}`}>
                 <Component {...pageProps} />
             </main>
         </>
